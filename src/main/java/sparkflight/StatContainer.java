5e1c4b2e32cb1f077455e3cbde74286931c44055
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 13:53:50 +0300

    Scripted auto-commit on change (2021-12-17 13:53:50) by gitwatch.sh

--- src/main/java/sparkflight/StatContainer.java
+++ src/main/java/sparkflight/StatContainer.java
@@ -45,6 +45,6 @@ public class StatContainer implements Serializable {
 
     }
     public String compareStat() {
-
+        return String.format(PATTERN, max, ((float) cancelled) / counter * 100, ((float) delayed) / counter * 100);
     }
 }
