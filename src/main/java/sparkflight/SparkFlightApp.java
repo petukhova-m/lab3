2c229215fe81d09f3d107bac890aedcf8d8a2df9
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 13:45:20 +0300

    Scripted auto-commit on change (2021-12-17 13:45:20) by gitwatch.sh

--- src/main/java/sparkflight/SparkFlightApp.java
+++ src/main/java/sparkflight/SparkFlightApp.java
@@ -19,7 +19,7 @@ public class SparkFlightApp {
 
     private static final String QUOTE = "\"";
     private static JavaRDD<String> dataPrepare(JavaSparkContext sc, String arg) {
-
+        JavaRDD<String> data = sc.textFile(arg);
         String dataHeader = data.first();
         data = data.filter(x -> !x.equals(dataHeader));
         return data;
