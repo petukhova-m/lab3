cb9e1a07f074f478c47ba7c8d69967e4582d1a2e
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 14:04:35 +0300

    Scripted auto-commit on change (2021-12-17 14:04:35) by gitwatch.sh

--- src/main/java/sparkflight/FlightSerializable.java
+++ src/main/java/sparkflight/FlightSerializable.java
@@ -9,7 +9,7 @@ public class FlightSerializable implements Serializable {
 
     public FlightSerializable(Integer originAirportId, Integer destinationAirportId, float delay, boolean cancelled) {
         this.originAirportId = originAirportId;
-
+        this.destinationAirportId = destinationAirportId;
         this.delay = delay;
         this.cancelled = cancelled;
     }
@@ -19,7 +19,7 @@ public class FlightSerializable implements Serializable {
 
     public Integer getDestinationAirportId() {
         return destinationAirportId;
-    }
+
 
     public Integer getOriginAirportId() {
         return originAirportId;
