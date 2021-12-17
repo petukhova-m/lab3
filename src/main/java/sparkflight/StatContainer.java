c1400187e06eba76407fa85591a0179b9ac6d326
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 13:47:55 +0300

    Scripted auto-commit on change (2021-12-17 13:47:55) by gitwatch.sh

--- src/main/java/sparkflight/StatContainer.java
+++ src/main/java/sparkflight/StatContainer.java
@@ -38,7 +38,7 @@ public class StatContainer implements Serializable {
                 a.getDelayed() + (delayed > 0 ? 1 : 0),
 
 
-
+    public static StatContainer add(StatContainer a, StatContainer b) {
 
                 a.getCancelled() + b.getCancelled(),
 
