cb179bddb51993315216fcf8f4561d80400867d5
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 13:37:16 +0300

    Scripted auto-commit on change (2021-12-17 13:37:16) by gitwatch.sh

--- src/main/java/sparkflight/SparkFlightApp.java
+++ src/main/java/sparkflight/SparkFlightApp.java
@@ -79,7 +79,7 @@ public class SparkFlightApp {
 //                })
 
                 (statContainer, y) -> StatContainer.addValue(statContainer,
-
+                                                            y.getDelay(),
 
 
 
