8f4c45522e63569373291a360c72949c93f88c5a
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 13:59:29 +0300

    Scripted auto-commit on change (2021-12-17 13:59:29) by gitwatch.sh

--- src/main/java/sparkflight/Constants.java
+++ src/main/java/sparkflight/Constants.java
@@ -2,7 +2,7 @@
 public class Constants {
     public static final int ARR_DELAY = 18;
     public static final int DESTINATION_AIRPORT_ID = 14;
-
+    public static final int ORIGIN_AIRPORT_ID = 11;
 
 
 
