8dfbc529c7d97bca337d915cb9405e4a0aa4b2cb
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 13:37:06 +0300

    Scripted auto-commit on change (2021-12-17 13:37:06) by gitwatch.sh

--- src/main/java/sparkflight/SparkFlightApp.java
+++ src/main/java/sparkflight/SparkFlightApp.java
@@ -2,7 +2,7 @@ package sparkflight;
 
 
 import org.apache.spark.api.java.JavaPairRDD;
-
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 
